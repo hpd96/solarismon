c74e552a9d0c552c5dc66b9b6a513685e48c0288
Peter H. Demus <peter@demus.de>
2016-03-07 11:31:46 +0100

    serial line error check while parsing line
    Signed-off-by: Peter H. Demus <peter@demus.de>

--- solaris.js
+++ solaris.js
@@ -1,35 +1,58 @@
+// SolarisMon ROTEX RPS3
+//   read measurements from serial port "CONF" and create MQTT messages
+
+// tested with: "R3 V.4.0"
+
+// sample:
+//
+// SOLARIS R3 V.4.0 Nov  4 2010 13:23:29 ROTEX GmbH Ser.-Nr: 000001273683
+// Zyklus /s:30
+// HA;BK;P1 /%;P2;TK /°C;TR /°C;TS /°C;TV /°C;V /l/min;ERROR;P/W
+// 0;0;0;0;38;38;44;31;0,0;;0
+
+var sSerialPort = "/dev/ttyAMA0";
+
 var fs = require('fs');
 var format = require('util').format;
 var serialport = require("serialport");
 var mqtt = require('mqtt');
 
-var mqttClient  = mqtt.connect('mqtt://tux.home');
+var mqttClient  = mqtt.connect('mqtt://localhost');
 var SerialPort = serialport.SerialPort;
 
 var baseFileName = '/home/pi/solarismon/solarismon-store';
 var mqttConnected = false;
 
-var lineMapping = ['HA', 'BSK', 'P1', 'P2', 'TK', 'TR', 'TS', 'TV', 'V', 'Status', 'P'];  
+var lineMapping = ['HA', 'BSK', 'P1', 'P2', 'TK', 'TR', 'TS', 'TV', 'V', 'Status', 'P'];
 
-var serial = new SerialPort("/dev/ttyAMA0", {
+var serial = new SerialPort(  sSerialPort , {
   baudrate: 19200,
   parser: serialport.parsers.readline("\r")
 });
 
 var bStoreTextFile = false;
+var bVerbose = false;
+
 var mqttTopic = "sensors/solaris"
 process.env.TZ = 'Europe/Berlin';
 
 
 serial.on("open", function () {
-  console.log('open');
+  console.log('serial open');
   serial.on('data', function(line) {
+    // drop CR or LF
     line = line.replace(/(\r\n|\n|\r)/gm,"");	
     var data = parseLine(line);
-    writeFile(data);
-    data = adjustFormat(data);
-    mqttClient.publish(mqttTopic, JSON.stringify(data)); 
-    console.log(data);	
+    if (data != null )
+    {
+      writeFile(data);
+      data = adjustFormat(data);
+      mqttClient.publish(mqttTopic, JSON.stringify(data));
+      if ( bVerbose == true )
+      {
+        console.log(data);
+      }
+    } // parseable data?
   });
 });
 
@@ -74,19 +97,39 @@ function writeFile(data){
     }
 }
 
+function IsNumeric(num) {
+     return (num >=0 || num < 0);
+}
 
 function parseLine(line) {
-    var result = {date: new Date()};
+    var result = null;
     if (typeof line == 'string' || line instanceof String) {
   	var parts = line.split(';', lineMapping.length);
 	if(parts.length === lineMapping.length) {
+            result = {date: new Date()};
             result.line = line;
 	    parts.forEach(function(p, i) {
 		var name = lineMapping[i] ==='?' ? 'var' + i : lineMapping[i];
 		result[name] = p;
 	    });
-	}
-    }
+            // avoid  { P1: 'P1 /%' } or {  HA: '§$%&/()' }
+            if ( ! IsNumeric( result['HA'] ) ) { return null; }
+            if ( ! IsNumeric( result['TS'] ) ) { return null; }
+            if ( ! IsNumeric( result['P1'] ) ) { return null; }
+            if ( ! IsNumeric( result['P'] ) ) { return null; }
+	} // no ';'
+	else
+	{
+          if ( line.indexOf('ROTEX')>=0 || line.indexOf('Zyklus')>=0 )
+          {
+            console.log( line );
+          }
+          else
+          {
+	    console.log( 'parser problem ' + line );
+          }
+        }
+    } // is a string
     return result;
 }
 
@@ -97,7 +140,5 @@ function adjustFormat(data) {
     }
     return data;
 }
-
-
 
 
