7127e0fb32e36f80cd2b8326145386f93b712405
Peter H. Demus <peter@demus.de>
2016-11-25 20:46:51 +0000

    update systemd

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,9 +1,11 @@
 sudo cp node-solarismon.service /etc/systemd/system
+sudo systemctl daemon-reload
 sudo systemctl enable node-solarismon
 sudo systemctl start node-solarismon
 sudo systemctl status node-solarismon
 
 sudo cp openhab.service /etc/systemd/system
+sudo systemctl daemon-reload
 sudo systemctl enable openhab
 sudo systemctl start openhab
 sudo systemctl status openhab
